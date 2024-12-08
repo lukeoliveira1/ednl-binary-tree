a7d82eab57ec436cc09143902b87688ffa34baf9
Lucas <lucasoliveirars22@gmail.com>
2024-12-08 12:07:59 -0300

    new activity

--- functions.py
+++ functions.py
@@ -1,26 +1,31 @@
 import tkinter as tk
+from queue import Queue
+
 
 class Node:
     def __init__(self, data) -> None:
         self.data = data
-        self.left : Node | None = None
-        self.right : Node | None = None
+        self.left: Node | None = None
+        self.right: Node | None = None
+
 
 # A
 
-def insert_node(root : Node, node : Node):
-    if node.data > root.data:    
-        if root.right is None:    
+
+def insert_node(root: Node, node: Node):
+    if node.data > root.data:
+        if root.right is None:
             root.right = node
         else:
-            insert_node(root.right, node) 
-    else: 
+            insert_node(root.right, node)
+    else:
         if root.left is None:
             root.left = node
         else:
-            insert_node(root.left, node) 
-        
+            insert_node(root.left, node)
+
     return root
+
 
 def create_tree(numbers):
     root = Node(numbers[0])
@@ -28,47 +33,170 @@ def create_tree(numbers):
         insert_node(root, Node(i))
     return root
 
+
 # B
 
-def search_tree(root : Node, number):
+
+def search_tree(root: Node, number):
     if root is None:
-        print("Não possui esse valor na árvore")
-        return
-    
+        return False
+
     if root.data == number:
-        print(f"{root.data} foi encontrado")
-        return
+        return True 
     elif number < root.data:
-        search_tree(root.left, number)
+        return search_tree(root.left, number)
     else:
-        search_tree(root.right, number)
+        return search_tree(root.right, number)
+
 
 # C
-def print_tree(root: Node):
+
+
+def print_tree(root: Node, level):
     if root == None:
         return "Árvore vazia"
-    
-    print(root.data)
-    print_tree(root.left)
-    print_tree(root.right)
+
+    if root.left:
+        print_tree(root.left, level+3)
+
+    print("  " * level + str(root.data))
+
+    if root.right:
+        print("")
+        print_tree(root.right, level+3)
+
 
 # D
-def count_nodes(root:Node):
+
+
+def count_nodes(root: Node):
     if root is None:
         return 0
     else:
         return 1 + count_nodes(root.left) + count_nodes(root.right)
 
+
 # E
-def count_leaf(root:Node):
+
+
+def count_leaf(root: Node):
     if root is None:
         return 0
     elif root.left is None and root.right is None:
         return 1
     else:
         return count_leaf(root.left) + count_leaf(root.right)
-    
-# Draw
+
+
+# F
+
+
+def search_successor(current: Node):
+    current = current.right
+    while current and current.left:
+        current = current.left
+    return current
+
+
+def delete_node(root: Node, to_remove):
+    # base case
+    if root is None:
+        return root
+
+    # chegar no node para deletar
+    elif to_remove < root.data:
+        root.left = delete_node(root.left, to_remove)
+    # chegar no node para deletar
+    elif to_remove > root.data:
+        root.right = delete_node(root.right, to_remove)
+
+    else:
+        # chegou no node para ser deletado
+
+        # 0 ou 1 filhos
+        if root.left is None:
+            return root.right # se não tem filho, vai retornar None
+        if root.right is None:
+            return root.left # se não tem filho, vai retornar None
+        
+        # pior caso, quando tem dois filhos
+        succ: Node = search_successor(root)
+        root.data = succ.data
+        # deletar o valor repetido
+        root.right = delete_node(root.right, succ.data)
+
+    return root
+
+
+# G
+
+
+#     usar fila
+#     coloca A na fila
+#     adicionar filhos de A
+#     remove A
+#     adiciona filhos de B
+#     remove B
+#     adiciona filhos de C
+#     remove C
+
+
+def visit(root: Node):
+    print(root.data)
+
+
+def levels_path(root: Node):
+    if not root:
+        return
+
+    queue = Queue()
+    queue.put(root)
+
+    while not queue.empty():
+        current = queue.get()
+        visit(current)
+
+        if current.left:
+            queue.put(current.left)
+        if current.right:
+            queue.put(current.right)
+
+
+# H
+
+
+def pre_order(root: Node):
+    visit(root)
+
+    if root.left is not None:
+        pre_order(root.left)
+
+    if root.right is not None:
+        pre_order(root.right)
+
+
+def simetric(root: Node):
+    if root.left is not None:
+        simetric(root.left)
+
+    visit(root)
+
+    if root.right is not None:
+        simetric(root.right)
+
+
+def post_order(root: Node):
+    if root.left is not None:
+        post_order(root.left)
+
+    if root.right is not None:
+        post_order(root.right)
+
+    visit(root)
+
+
+# draw
+
 
 def draw_tree(canvas, node, x, y, dx):
     if node is None:
@@ -85,6 +213,7 @@ def draw_tree(canvas, node, x, y, dx):
     if node.right:
         canvas.create_line(x, y, x + dx, y + 50, arrow=tk.LAST)
         draw_tree(canvas, node.right, x + dx, y + 50, dx // 2)
+
 
 def display_tree(root):
     window = tk.Tk()
--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from functions import *
 numbers = [100, 50, 200, 70, 140, 30, 350, 117, 400, 42, 80, 65]
 
 root = create_tree(numbers)
-search_tree(root, 117)
-# print_tree(root)
-print(f"Possui {count_nodes(root)} nós")
-print(f"Possui {count_leaf(root)} folhas")
+
+# print("Existe na árvore?", search_tree(root, 117))
+# print_tree(root, 0)
+# print(f"Possui {count_nodes(root)} nós")
+# print(f"Possui {count_leaf(root)} folhas")
 # display_tree(root)
+
+# new activity
+
+# levels_path(root)
+
+# root = delete_node(root, 100)
+
+# display_tree(root)
+
+# pre_order(root)
+# simetric(root)
+# post_order(root)
